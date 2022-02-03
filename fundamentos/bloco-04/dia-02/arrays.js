5ebb01e84af89a11d9491fe4455b9361bc51ea57
Wellington Ferreira <wantunesferreira@gmail.com>
2022-02-03 19:32:10 -0300

    resolução dos exemplos e exercicios

--- fundamentos/bloco-04/dia-02/arrays.js
+++ fundamentos/bloco-04/dia-02/arrays.js
@@ -1,3 +1,4 @@
+/* -------------------- Exemplos -------------------- 
 let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
 
 console.log(tasksList.length);
@@ -19,3 +20,23 @@ tasksList.push('Fazer exercícios da Trybe');  // adiciona mais uma tarefa
 console.log(tasksList);
 
 // ['Tomar café', 'Reunião', 'Brincar com o cachorro', 'Fazer exercícios da Trybe']
+-------------------- Exemplos -------------------- */
+
+/* -------------------- Exercício 1 -------------------- */
+// Obtenha o valor "Serviços" do array menu:
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+
+/* -------------------- Exercício 2 -------------------- */
+// Procure o índice do valor "Portfólio" do array menu :
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
+
+/* -------------------- Exercício 3 -------------------- */
+// Adicione o valor "Contato" no final do array menu :
+menu.push('Contato');
+
+console.log(menu);

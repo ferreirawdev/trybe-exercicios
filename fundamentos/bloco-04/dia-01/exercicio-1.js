ba4085f996d0f17f2ffd8f61f370f77a7b060470
Wellington Ferreira <wantunesferreira@gmail.com>
2022-02-03 11:53:42 -0300

    adiciona requisitos

---
+++ fundamentos/bloco-04/dia-01/exercicio-1.js
@@ -0,0 +1,17 @@
+const a = 72;
+const b = 18;
+
+// Programa 1
+console.log(a + b);
+
+// Programa 2
+console.log(a - b);
+
+// Programa 1
+console.log(a * b);
+
+// Programa 1
+console.log(a / b);
+
+// Programa 1
+console.log(a % b);

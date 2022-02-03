89949006cd6871e903291161e890564374025584
Wellington Ferreira <wantunesferreira@gmail.com>
2022-02-03 11:12:16 -0300

    adiciona requisitos do 1 ao 4

---
+++ fundamentos/bloco-04/dia-01/pratica3.js
@@ -0,0 +1,29 @@
+const nota = 90;
+
+if (nota >= 80) {
+    console.log('Parabéns, você foi aprovada(o)!');
+} else if (nota < 80 && nota >= 60) {
+    console.log('Você está na nossa lista de espera');
+} else {
+    console.log('Você foi reprovada(o)');
+}
+
+const nota2 = 70;
+
+if (nota2 >= 80) {
+    console.log('Parabéns, você foi aprovada(o)!');
+} else if (nota2 < 80 && nota2 >= 60) {
+    console.log('Você está na nossa lista de espera');
+} else {
+    console.log('Você foi reprovada(o)');
+}
+
+const nota3 = 40;
+
+if (nota3 >= 80) {
+    console.log('Parabéns, você foi aprovada(o)!');
+} else if (nota3 < 80 && nota3 >= 60) {
+    console.log('Você está na nossa lista de espera');
+} else {
+    console.log('Você foi reprovada(o)');
+}

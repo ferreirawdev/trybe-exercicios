95af55a4bb793df7ca271583c0ca41949cdbef07
Wellington Ferreira <wantunesferreira@gmail.com>
2022-02-03 12:04:59 -0300

    adiciona requisitos

---
+++ fundamentos/bloco-04/dia-01/exercicio-2.js
@@ -0,0 +1,32 @@
+const num1 = 72;
+const num2 = 18;
+
+if (num1 > num2){
+    console.log(num1);
+} else if (num1 < num2){
+    console.log(num2);
+} else {
+    console.log('São iguais');
+}
+
+num3 = 72;
+num4 = 81;
+
+if (num3 > num4){
+    console.log(num3);
+} else if (num3 < num4){
+    console.log(num4);
+} else {
+    console.log('São iguais');
+}
+
+num5 = 72;
+num6 = 72;
+
+if (num5 > num6){
+    console.log(num5);
+} else if (num5 < num6){
+    console.log(num6);
+} else {
+    console.log('São iguais');
+}

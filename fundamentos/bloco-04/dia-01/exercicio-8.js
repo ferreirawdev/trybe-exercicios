bc1eb98c9790b9614e681dabfbd09bfc828c2f8d
Wellington Ferreira <wantunesferreira@gmail.com>
2022-02-03 15:54:29 -0300

    adiciona requisitos

---
+++ fundamentos/bloco-04/dia-01/exercicio-8.js
@@ -0,0 +1,63 @@
+const num1 = 80;
+const num2 = 39;
+const num3 = 65;
+
+if ((num1 % 2) === 0) {
+    console.log('true');
+} else if ((num2 % 2) === 0) {
+    console.log('true');
+} else if ((num3 % 2) === 0) {
+    console.log('true');
+} else {
+    console.log('false');
+}
+
+/* -*-*-*-*-* Bônus -*-*-*-*-*- */
+if ((num1 % 2) === 0 || (num2 % 2) === 0 || (num3 % 2) === 0) {
+    console.log('true');
+} else {
+    console.log('false');
+}
+/* -*-*-*-*-* Bônus -*-*-*-*-*- */
+
+const num4 = 81;
+const num5 = 38;
+const num6 = 63;
+
+if ((num4 % 2) === 0) {
+    console.log('true');
+} else if ((num5 % 2) === 0) {
+    console.log('true');
+} else if ((num6 % 2) === 0) {
+    console.log('true');
+} else {
+    console.log('false');
+}
+
+const num7 = 81;
+const num8 = 37;
+const num9 = 64;
+
+if ((num7 % 2) === 0) {
+    console.log('true');
+} else if ((num8 % 2) === 0) {
+    console.log('true');
+} else if ((num9 % 2) === 0) {
+    console.log('true');
+} else {
+    console.log('false');
+}
+
+const num10 = 3;
+const num11 = 5;
+const num12 = 7;
+
+if ((num10 % 2) === 0) {
+    console.log('true');
+} else if ((num11 % 2) === 0) {
+    console.log('true');
+} else if ((num12 % 2) === 0) {
+    console.log('true');
+} else {
+    console.log('false');
+}
---
+++ fundamentos/bloco-04/dia-01/exercicio-9.js
@@ -0,0 +1,63 @@
+const num1 = 81;
+const num2 = 38;
+const num3 = 64;
+
+if ((num1 % 2) !== 0) {
+    console.log('true');
+} else if ((num2 % 2) !== 0) {
+    console.log('true');
+} else if ((num3 % 2) !== 0) {
+    console.log('true');
+} else {
+    console.log('false');
+}
+
+/* -*-*-*-*-* Bônus -*-*-*-*-*- */
+if ((num1 % 2) !== 0 || (num2 % 2) !== 0 || (num3 % 2) !== 0) {
+    console.log('true');
+} else {
+    console.log('false');
+}
+/* -*-*-*-*-* Bônus -*-*-*-*-*- */
+
+const num4 = 80;
+const num5 = 37;
+const num6 = 64;
+
+if ((num4 % 2) !== 0) {
+    console.log('true');
+} else if ((num5 % 2) !== 0) {
+    console.log('true');
+} else if ((num6 % 2) !== 0) {
+    console.log('true');
+} else {
+    console.log('false');
+}
+
+const num7 = 80;
+const num8 = 38;
+const num9 = 65;
+
+if ((num7 % 2) !== 0) {
+    console.log('true');
+} else if ((num8 % 2) !== 0) {
+    console.log('true');
+} else if ((num9 % 2) !== 0) {
+    console.log('true');
+} else {
+    console.log('false');
+}
+
+const num10 = 2;
+const num11 = 4;
+const num12 = 6;
+
+if ((num10 % 2) !== 0) {
+    console.log('true');
+} else if ((num11 % 2) !== 0) {
+    console.log('true');
+} else if ((num12 % 2) !== 0) {
+    console.log('true');
+} else {
+    console.log('false');
+}

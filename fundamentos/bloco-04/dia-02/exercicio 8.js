2cba4125311ab62e50bce0c132759c08ce365225
Wellington Ferreira <wantunesferreira@gmail.com>
2022-02-04 09:13:45 -0300

    adiciona resolucao

---
+++ fundamentos/bloco-04/dia-02/exercicio 8.js
@@ -0,0 +1,10 @@
+/* --------------- Exercício 8 --------------- */
+// Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;
+
+let meuArray = [];
+
+for (let i = 0; i < 25; i += 1){
+    meuArray.push(i + 1);
+}
+
+console.log(meuArray);

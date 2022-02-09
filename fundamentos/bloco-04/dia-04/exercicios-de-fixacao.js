1cd53c9a72246aa2b4b5417b21102debae3b1f9a
Wellington Ferreira <wantunesferreira@gmail.com>
2022-02-08 22:53:23 -0300

    adiciona resolucao

--- fundamentos/bloco-04/dia-04/exercicios-de-fixacao.js
+++ fundamentos/bloco-04/dia-04/exercicios-de-fixacao.js
@@ -41,3 +41,31 @@ console.log('A jogadora ' + player.name + ' ' + player.lastName + ' foi eleita a
 // "A jogadora possui 2 medalhas de ouro e 3 medalhas de prata".
 
 console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata.');
+
+/* ------------- FOR/IN e FOR/OF ------------- */
+/* --------------- Exercício 1 --------------- */
+// Usando o objeto abaixo, utilize for/in e imprima no console a mensagem
+// 'Olá xxxxx' para cada nome, substituindo o xxxxx pelo nome em questão.
+
+let names = {
+  person1: 'João',
+  person2: 'Maria',
+  person3: 'Jorge',
+};
+
+for (let name in names){
+    console.log('Olá, ' + names[name]);
+}
+
+/* --------------- Exercício 2 --------------- */
+// Usando o objeto abaixo, utilize for/in e imprima um console.log com as chaves e valores desse objeto.
+
+let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+};
+
+for (let key in car){
+    console.log(key + ': ' + car[key]);
+}

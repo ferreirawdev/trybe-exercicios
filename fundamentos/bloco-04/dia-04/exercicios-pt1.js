a1c13757ac559ab4a779c45c35b89e379d3420e1
Wellington Ferreira <wantunesferreira@gmail.com>
2022-02-09 10:58:39 -0300

    adiciona resolucao

--- fundamentos/bloco-04/dia-04/exercicios-pt1.js
+++ fundamentos/bloco-04/dia-04/exercicios-pt1.js
@@ -1,24 +1,38 @@
 // Usando o objeto abaixo, faça os exercícios a seguir:
 
 let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-;}
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
 
 /* --------------- Exercício 1 --------------- */
 // Imprima no console uma mensagem de boas-vindas para a personagem acima,
 // incluindo seu nome.
 
+console.log('Bem-vinda, ' + info.personagem);
+
 /* --------------- Exercício 2 --------------- */
 // Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o
 // valor 'Sim' e, em seguida, imprima o objeto no console.
 
+info.recorrente = 'Sim';
+
+console.log(info);
+
 /* --------------- Exercício 3 --------------- */
 // Faça um for/in que mostre todas as chaves do objeto.
 
+for (let key in info){
+  console.log(key);
+}
+
 /* --------------- Exercício 4 --------------- */
 // Faça um novo for/in , mas agora mostre todos os valores das chaves do objeto.
+
+for (key in info){
+  console.log(info[key]);
+}
 
 /* --------------- Exercício 5 --------------- */
 // Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do
@@ -26,25 +40,42 @@ let info = {
 // Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima
 // os valores de cada objeto juntos de acordo com cada uma das chaves.
 
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim',
+}
+
+for (key in info){
+  if (key === 'recorrente' && info[key] === 'Sim' && info2[key] === 'Sim'){
+    console.log('Ambos recorrentes');
+  } else {
+    console.log(info[key] + ' e ' + info2[key]);
+  }
+}
+
 // Usando o objeto abaixo, faça os exercícios a seguir:
 
 let leitor = {
-    nome: 'Julia',
-    sobrenome: 'Pessoa',
-    idade: 21,
-    livrosFavoritos: [
-      {
-        titulo: 'O Pior Dia de Todos',
-        autor: 'Daniela Kopsch',
-        editora: 'Tordesilhas',
-      },
-    ],
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
 }
 
 /* --------------- Exercício 6 --------------- */
 // Acesse as chaves nome, sobrenome e titulo, que está dentro da chave
 // livrosFavoritos, e faça um console.log no seguinte formato:
 // "O livro favorito de Julia Pessoa se chama 'O Pior Dia de Todos'".
+
+console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama "' + leitor.livrosFavoritos[0].titulo + '"');
 
 /* --------------- Exercício 7 --------------- */
 // Adicione um novo livro favorito na chave livrosFavoritos, que é um array.
@@ -56,6 +87,14 @@ let leitor = {
 //     editora: 'Rocco',
 // }
 
+leitor.livrosFavoritos.push({
+  titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+  autor: 'JK Rowling',
+  editora: 'Rocco',
+});
+
 /* --------------- Exercício 8 --------------- */
 // Acesse as chaves nome e livrosFavoritos e faça um console.log no seguinte
 // formato: "Julia tem 2 livros favoritos".
+
+console.log(leitor.nome + ' tem ' + leitor.livrosFavoritos.length + ' livros favoritos.');

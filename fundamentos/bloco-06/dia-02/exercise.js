d759d8ea50978f8adca26eeb29d9d472c7752e01
Wellington Ferreira <wantunesferreira@gmail.com>
2022-02-28 17:54:49 -0300

    Adiciona resolucao dos exercicios bonus

--- fundamentos/bloco-06/dia-02/exercise.js
+++ fundamentos/bloco-06/dia-02/exercise.js
@@ -1,36 +1,21 @@
 const submitButton = document.querySelector("#submit-btn");
 const agreement = document.querySelector("#agreement");
-const fullName = document.querySelector("#full-name");
-const email = document.querySelector("#email");
-const reason = document.querySelector("#why");
-
-function textInputValidation() {
-  const nameLen = fullName.value.length;
-  const emailLen = email.value.length;
-  const reasonLen = reason.value.length;
-  const invalidName = nameLen < 10 || nameLen > 40;
-  const invalidEmail = emailLen < 10 || emailLen > 50;
-  const invalidReason = reasonLen > 500;
-
-  if (invalidName || invalidEmail || invalidReason) {
-    return false;
-  } else {
-    return true;
-  }
-}
-
-function preventSubmit(event) {
-  event.preventDefaut();
-  const validation = textInputValidation();
-
-  if (validation === false) {
-    alert("Dados inválidos");
-  } else {
-    alert(
-      "Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip."
-    );
-  }
-}
+const validate = new JustValidate("#form", {
+  errorFieldCssClass: "is-invalid",
+  errorFieldStyle: {
+    border: "1px solid red",
+  },
+  errorLabelCssClass: "is-label-invalid",
+  errorLabelStyle: {
+    color: "red",
+    textDecoration: "underlined",
+  },
+  focusInvalidField: true,
+  lockForm: true,
+  tooltip: {
+    position: "top",
+  },
+});
 
 function enableSubmitButton() {
   if (agreement.checked) {
@@ -38,7 +23,74 @@ function enableSubmitButton() {
   }
 }
 
+const picker = new Pikaday({
+  field: document.getElementById("date"),
+  format: "D/M/YYYY",
+  toString(date, format) {
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  },
+  parse(dateString, format) {
+    const parts = dateString.split("/");
+    const day = parseInt(parts[0], 10);
+    const month = parseInt(parts[1], 10) - 1;
+    const year = parseInt(parts[2], 10);
+    return new Date(year, month, day);
+  },
+});
+
+validate
+  .addField("#fullName", [
+    {
+      rule: "required",
+      errorMessage: "O campo de nome é obrigatório.",
+    },
+    {
+      rule: "maxLength",
+      value: 40,
+      errorMessage: "O limite é de 40 caracteres.",
+    },
+    {
+      rule: "minLength",
+      value: 10,
+      errorMessage: "O mínimo é de 10 caracteres.",
+    },
+  ])
+  .addField("#email", [
+    {
+      rule: "required",
+      errorMessage: "O campo de email é obrigatório.",
+    },
+    {
+      rule: "email",
+      errorMessage: "O email digitado não é válido.",
+    },
+    {
+      rule: "maxLength",
+      value: 50,
+      errorMessage: "O limite é de 50 caracteres.",
+    },
+  ])
+  .addField("#why", [
+    {
+      rule: "required",
+      errorMessage: "O campo de resposta é obrigatório.",
+    },
+    {
+      rule: "maxLength",
+      value: 500,
+      errorMessage: "O limite é de 500 caracteres",
+    },
+  ])
+  .addField("#date", [
+    {
+      rule: "required",
+      errorMessage: "O campo de data é obrigatório.",
+    },
+  ]);
+
 window.onload = function load() {
-  submitButton.addEventListener("click", preventSubmit);
-  agreement.addEventListener("change", enableSubmitButton);
+  agreement.addEventListener('change', enableSubmitButton);
 };

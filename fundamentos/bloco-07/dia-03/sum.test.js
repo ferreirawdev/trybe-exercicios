0a15f49056835a4af49e5bcab3007e13d72ed699
Wellington Ferreira <wantunesferreira@gmail.com>
2022-03-10 09:40:40 -0300

    adiciona resolucao

--- fundamentos/bloco-07/dia-03/sum.test.js
+++ fundamentos/bloco-07/dia-03/sum.test.js
@@ -1,5 +1,12 @@
-const sum = (a, b) => a + b;
+const sum = require("./sum.js");
 
-test('sums two values', () => {
-  expect(sum(2, 3)).toEqual(5);
+describe("testa a função sum", () => {
+  it("deve retornar o resultado da soma", () => {
+    expect(sum(4, 5)).toBe(9);
+    expect(sum(0, 0)).toBe(0);
+  });
+  it("deve disparar um erro caso receba como parâmetro uma string", () => {
+    expect(() => sum(4, "5")).toThrowError();
+    expect(() => sum(4, "5")).toThrowError("parameters must be numbers");
+  });
 });
